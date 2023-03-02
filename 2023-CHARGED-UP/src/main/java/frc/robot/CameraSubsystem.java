e59cb899cbabcb253d61e2ad160c74964c6fcfdb
NCPS FRC Team 1908 <54084750+Solar-Geaux@users.noreply.github.com>
2023-03-02 16:57:45 -0500

    camera subsystem started, put values into smartdashboard

---
+++ 2023-CHARGED-UP/src/main/java/frc/robot/CameraSubsystem.java
@@ -0,0 +1,40 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+
+public class CameraSubsystem extends SubsystemBase{
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry ty = table.getEntry("ty");
+    NetworkTableEntry ta = table.getEntry("ta");
+
+    
+
+public CameraSubsystem() {
+
+    }
+
+@Override
+public void periodic(){
+    //read values periodically
+    double x = tx.getDouble(0.0);
+    double y = ty.getDouble(0.0);
+    double area = ta.getDouble(0.0);
+
+    //post to smart dashboard periodically
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea", area);
+    }
+}
--- 2023-CHARGED-UP/src/main/java/frc/robot/RobotContainer.java
+++ 2023-CHARGED-UP/src/main/java/frc/robot/RobotContainer.java
@@ -144,6 +144,8 @@ public class RobotContainer {
 
   }
 
+
+
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
