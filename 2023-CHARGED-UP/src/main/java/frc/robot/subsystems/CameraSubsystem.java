cbc7a47bc20437fbf6dfe2bf43f00563e7096910
NCPS FRC Team 1908 <54084750+Solar-Geaux@users.noreply.github.com>
2023-03-04 09:29:31 -0500

    moved camerasubsytem to subsystems folder destination

--- 2023-CHARGED-UP/src/main/java/frc/robot/subsystems/CameraSubsystem.java
+++ 2023-CHARGED-UP/src/main/java/frc/robot/subsystems/CameraSubsystem.java
@@ -1,12 +1,6 @@
-package frc.robot;
+package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.commands.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -27,12 +21,12 @@ public CameraSubsystem() {
 
 @Override
 public void periodic(){
-    //read values periodically
+   
     double x = tx.getDouble(0.0);
     double y = ty.getDouble(0.0);
     double area = ta.getDouble(0.0);
 
-    //post to smart dashboard periodically
+  
     SmartDashboard.putNumber("LimelightX", x);
     SmartDashboard.putNumber("LimelightY", y);
     SmartDashboard.putNumber("LimelightArea", area);
