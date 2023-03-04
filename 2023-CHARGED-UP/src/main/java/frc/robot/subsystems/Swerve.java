b974470c451c2cb19f3347c8468c5d07d531bebe
TimRCAnderson <timothyrcanderson@gmail.com>
2023-03-04 12:24:19 -0500

    Swerve subsystem puts reported gyro yaw on dashboard to help with debugging gyro calibration.

--- 2023-CHARGED-UP/src/main/java/frc/robot/subsystems/Swerve.java
+++ 2023-CHARGED-UP/src/main/java/frc/robot/subsystems/Swerve.java
@@ -137,6 +137,9 @@ public class Swerve extends SubsystemBase {
     public void periodic(){
         swerveOdometry.update(getYaw(), getModulePositions());  
 
+        //put gyro yaw on the dashboard to help with calibration.
+        SmartDashboard.putNumber("Reported Yaw", gyro.getYaw());
+
         for(SwerveModule mod : mSwerveMods){
             SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Cancoder", mod.getCanCoder().getDegrees());
             SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Integrated", mod.getPosition().angle.getDegrees());
